5941833b986659c400194218f5bdf35edd491a3a
Maki Hirose <makidigital.sync@gmail.com>
2020-01-24 11:11:04 -0500

    sample.js updated

--- fusion/node/sample.js
+++ fusion/node/sample.js
@@ -4,10 +4,11 @@ const yelp = require('yelp-fusion');
 
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-const apiKey = '<YOUR_API_KEY>';
+const apiKey = '28h_TF5B3iOa8CBn1FW6DhEMUAmdunA2MsoL1tAtujspOU6IK34wsaV3d40UqpoLnv1-NVWVH7um28z1FMqPv64n_GGe6A4BiI3Dx-ybytgUeSVNXTFH98FfRoYSXXYx';
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
+  // term:'Four Barrel Coffee',
+  term:'Day care',
   location: 'san francisco, ca'
 };
 
